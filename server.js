f35306f07bb386b88b956851da1a37385a81261e
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-10-09 00:35:11 -0700

    .

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const RAW_FILE = id => `./raw_${id}.json`;
 const app = express();
 const port = process.env.PORT ?? 8000;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
